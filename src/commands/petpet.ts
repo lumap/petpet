ee47a42b302bdb4705d15b6780f9e32c99145f8c
lumap <lumap@duck.com>
2023-12-01 22:47:08 +0100

    Refactor petpet command and add logging

--- src/commands/petpet.ts
+++ src/commands/petpet.ts
@@ -7,7 +7,6 @@ import { logCommand } from "../functions/logs";
 let urlcheck = require('is-a-url');
 
 export async function petpet(interaction: APIChatInputApplicationCommandInteractionData, res: FastifyReply, ogInteraction: APIApplicationCommandInteraction) {
-    logCommand("petpet");
     let param = interaction.options![0];
     if (!interaction.options || param.type !== ApplicationCommandOptionType.Subcommand || !param.options) return sendMessage(res, {
         content: "What the fuck?",
@@ -20,12 +19,14 @@ export async function petpet(interaction: APIChatInputApplicationCommandInteract
             const avatarHash = interaction.resolved?.members?.[userId].avatar || interaction.resolved?.users?.[userId].avatar;
             url = avatarHash ? `https://cdn.discordapp.com/avatars/${userId}/${avatarHash}.png?size=1024` : `https://cdn.discordapp.com/embed/avatars/${(Number(userId) >> 22) % 6}.png?size=1024`;
             target = interaction.resolved?.users?.[userId].username || "someone, idk discord fucked up";
+            logCommand("petpet user");
             break;
         }
         case ApplicationCommandOptionType.Attachment: {
             const attachmentId = param.options[0].value;
             url = interaction.resolved?.attachments?.[attachmentId]!.url!;
             target = "an attachment";
+            logCommand("petpet attachment");
             break;
         }
         case ApplicationCommandOptionType.String: {
@@ -38,6 +39,7 @@ export async function petpet(interaction: APIChatInputApplicationCommandInteract
             }
             url = str;
             target = "an image url";
+            logCommand("petpet imageurl");
             break;
         }
     }
