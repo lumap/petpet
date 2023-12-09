49a73b162d2ad445449a89fe65f1fd9d974f641f
Just a Regular Talking Box <51658307+Koumes2@users.noreply.github.com>
2023-12-09 18:43:27 +0100

    fixing

--- src/commands/index.ts
+++
@@ -1,7 +0,0 @@
-import { invite } from "./invite";
-import { petpet } from "./petpet";
-import { petpetMsgCtx } from "./petpetMsgCtx";
-import { petpetUserCtx } from "./petpetUserCtx";
-import { support } from "./support";
-
-export default { support, invite, petpet, petpetMsgCtx, petpetUserCtx };
